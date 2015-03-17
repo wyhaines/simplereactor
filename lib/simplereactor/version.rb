bdbe940b9abfba238f027e08cde10f4191b16d24
Kirk Haines <wyhaines@gmail.com>
2015-03-16 20:17:45 -0600

    Work in progress.

--- lib/simplereactor/version.rb
+++ lib/simplereactor/version.rb
@@ -1,7 +1,7 @@
 module SimpleReactor
     
   NAME = "simplereactor"
-  VERSION = "2.0.0_pre1"
+  VERSION = "2.0.0"
   AUTHORS = ["Kirk Haines"]
   EMAIL   = ["wyhaines@gmail.com"]
   SUMMARY =  %q{Simplereactor is a very simple, pure Ruby event based IO reactor, with no external requirements. Use it when all you need is some simple, no frills event based IO.}
